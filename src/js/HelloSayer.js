2f6468641f170ce0d30035388f25b48d4033b7f3
Patrick Steadman <ptsteadman@gmail.com>
2016-03-23 16:47:03 -0400

    Reconfigure paths for bundle/build

--- src/js/HelloSayer.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-var HelloSayer = React.createClass({
-    render: function() {
-        return (<p>Hello {this.props.name}!</p>);
-    }
-});
-
-module.exports = HelloSayer;
