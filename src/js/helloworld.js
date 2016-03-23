ae3ec8760e3b6cffc50d318a5a9aec07b16f2a1f
Patrick Steadman <ptsteadman@gmail.com>
2016-03-23 14:58:38 -0400

    Add webpack config for sass and babel

--- src/js/helloworld.js
+++ src/js/helloworld.js
@@ -1,2 +1,9 @@
 require('./hello');
 require('./world');
+
+var css = require("../sass/test.scss");
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(<h1>Hello</h1>, document.getElementById("content"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,24 @@
 module.exports = {
     entry: {
-        helloWorld: './src/js/helloworld'
+        helloWorld: [
+          'webpack-dev-server/client?http://localhost:8080',
+          './src/js/helloworld']
     },
     output: {
-        filename: 'dist/[name].js'
+        filename: 'public/[name].js'
+    },
+    module: {
+      loaders: [
+        { test: /\.js$/, 
+          exclude: /node_modules/,
+          loader: 'babel',
+          query: { presets: ['react',  'es2015'] }, 
+        },
+        { test: /\.scss$/, 
+          exclude: /node_modules/,
+          loaders: ["style", "css", "sass"],
+        }
+      ]
     }
+
 };
