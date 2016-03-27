3169f641b03f29a38ebcd27a9a20f6c94c305b97
Patrick Steadman <ptsteadman@gmail.com>
2016-03-27 08:53:38 -0400

    Add video player pages

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -33,7 +33,17 @@ var Header = React.createClass({
   }
 });
 
-var HomeCopy = React.createClass({
+var Footer = React.createClass({
+  render: function() {
+    return (
+      <div className="footer copy">
+        <h1>Verdes NYC</h1>
+      </div>
+    );
+  }
+});
+
+var Home = React.createClass({
   render: function(){
     return (
       <div className="copy"> 
@@ -51,16 +61,100 @@ var HomeCopy = React.createClass({
 var Explore = React.createClass({
   render: function(){
     return (
-      <h1>Explore</h1>
+      <div className="copy">
+        <h2>Explore</h2>
+        <Link className="button" to="/">Home</Link>
+        <Link className="button" to="/contact">Contact</Link>
+        <Link className="button" to="/problem">Video: The Problem</Link>
+        <Link className="button" to="/solution">Video: The Solution</Link>
+        <Link className="button" to="/marketspace">Video: Market Space</Link>
+        <Link className="button" to="/brandvolume">Video: Brand Volume</Link>
+      </div>
     );
   }
 });
 
-var Footer = React.createClass({
-  render: function() {
+var Contact = React.createClass({
+  render: function(){
     return (
-      <div className="footer">
-        <h1>Verdes NYC</h1>
+      <div className="copy"> 
+        <h2>
+        PinPoints is a tool created by <a href="http://verdes.nyc">Verdes</a> to
+        redefine how businesses visualize the market space and their place
+        within it.  To use PinPoints, get in touch with us.
+        </h2> 
+        <Link className="button" to="/contact">Contact</Link>
+      </div>
+    );
+  }
+});
+
+
+var VideoPlayer = React.createClass({
+  render: function(){
+    return (
+      <iframe src={"https://player.vimeo.com/video/" + this.props.videoId +
+        "?byline=0&portrait=0&title=0&autoplay=1"} width="500" height="281"
+        frameBorder="0" webkitallowfullscreen mozallowfullscreen allowFullScreen
+        className="videoPlayer"></iframe>
+    );
+  }
+});
+
+
+var VideoProblem = React.createClass({
+  render: function(){
+    return (
+      <div className="copy">
+        <h2>The Problem</h2>
+        <VideoPlayer videoId="140358722" />
+        <br />
+        <Link className="button" to="/explore">Explore</Link>
+      </div>
+    );
+  }
+});
+
+var VideoSolution = React.createClass({
+  render: function(){
+    return (
+      <div className="copy"> 
+        <h2>
+        The Solution
+        </h2> 
+        <VideoPlayer videoId="140358721" />
+        <br />
+        <Link className="button" to="/explore">Explore</Link>
+      </div>
+    );
+  }
+});
+
+var VideoMarketSpace = React.createClass({
+  render: function(){
+    return (
+      <div className="copy"> 
+        <h2>
+        Market Space
+        </h2> 
+        <VideoPlayer videoId="140359046" />
+        <br />
+        <Link className="button" to="/explore">Explore</Link>
+      </div>
+    );
+  }
+});
+
+var VideoBrandVolume = React.createClass({
+  render: function(){
+    return (
+      <div className="copy"> 
+        <h2>
+        Brand Volume
+        </h2> 
+        <VideoPlayer videoId="140687606" />
+        <br />
+        <Link className="button" to="/explore">Explore</Link>
       </div>
     );
   }
@@ -69,8 +163,13 @@ var Footer = React.createClass({
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={HomeCopy} />
+      <IndexRoute component={Home} />
       <Route path="explore" component={Explore} />
+      <Route path="contact" component={Contact} />
+      <Route path="problem" component={VideoProblem} />
+      <Route path="solution" component={VideoSolution} />
+      <Route path="marketspace" component={VideoMarketSpace} />
+      <Route path="brandvolume" component={VideoBrandVolume} />
     </Route>
   </Router>
 ), document.getElementById("app"));
